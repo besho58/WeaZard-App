bbc6bb38bf81c242ca3562e44ae0a3860ee1b250
Bashar Ahmed <basharahmed419@gmail.com>
2021-05-16 05:41:37 +0200

    Changed thr IP API to much saver one with (https://) 🚀

--- src/js/models/UserIP.js
+++ src/js/models/UserIP.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
+const apiKey = at_pToBWyomMwjUb5syXEnj7srYdeX2D;
+
 export default class {
     constructor(){}
     async findIP() {
         let IP;
         try {
-            IP = await axios('http://ip-api.com/json/');
+            IP = await axios(`https://geo.ipify.org/api/v1?apiKey=${apiKey}`);
         } catch (error) {
             alert(error);
         }
-        this.userCity = IP.data.city;
+        this.userCity = IP.location.city;
     }
 }
